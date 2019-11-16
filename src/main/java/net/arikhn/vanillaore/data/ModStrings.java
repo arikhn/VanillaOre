dcbe24f3f6d0c0cd012750a750067946e7729731
Arik Hardiansyah <57228859+ArikHn@users.noreply.github.com>
2019-11-17 00:50:52 +0700

    Create ModStrings.java

---
+++ src/main/java/net/arikhn/vanillaore/data/ModStrings.java
@@ -0,0 +1,16 @@
+package net.arikhn.vanillaore.data;
+
+/**
+ * @author Arik Hardiansyah Nugraha
+ */
+
+public class ModStrings 
+{
+	public static final String MODID = "vanillaore";
+	public static final String MODNAME = "VanillaOre";
+	public static final String MODVERSION = "1.0.0";
+}
+
+/**
+ * Created on 00:11:31 - 17 Nov 2019
+ */
