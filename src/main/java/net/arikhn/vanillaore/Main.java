e22bbb37baf1baaa4c19c9ff01c05f48f4c1d42e
Arik Hardiansyah <57228859+ArikHn@users.noreply.github.com>
2019-11-17 01:06:09 +0700

    Update Main.java

--- src/main/java/net/arikhn/vanillaore/Main.java
+++ src/main/java/net/arikhn/vanillaore/Main.java
@@ -4,7 +4,9 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import net.arikhn.vanillaore.data.ModStrings;
+import net.arikhn.vanillaore.util.ConfigHandler;
 import net.arikhn.vanillaore.world.WorldGenOres;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
@@ -23,9 +25,13 @@ public class Main
 	
 	public static final Logger Log = LogManager.getLogger("VANILLAORE");
 	
+	//Event Handler
+	public static ConfigHandler configHandler = new ConfigHandler();
+	
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event)
 	{
+		MinecraftForge.EVENT_BUS.register(configHandler);
 		GameRegistry.registerWorldGenerator(new WorldGenOres(), 3);
 	}
 }
