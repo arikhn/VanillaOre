7b437ae4176f3d7e254604a20b2dcb32d9f0887d
Arik Hardiansyah <57228859+ArikHn@users.noreply.github.com>
2020-11-01 13:20:05 +0700

    Updating License file

--- src/main/java/arikhn/vanillaore/world/WorldGenOres.java
+++ src/main/java/arikhn/vanillaore/world/WorldGenOres.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2019 Arik Hardiansyah Nugraha
+ * Copyright (C) 2019 ArikHn
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
